37876dd5a027298964a79d7f43aec1e9d56bdffd
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-03-12 20:45:11 +0100

    moved quiz to the server

---
+++ src/server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const matchApi = require('./routes/match-api')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api',matchApi);
+
+app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
+
+app.use((req, res, next) => {
+  if (req.method !== 'GET') return next();
+  res.sendFile(path.resolve(__dirname, '..', '..', 'dist', 'index.html'));
+});
+
+module.exports = app;
--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,7 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const app = require('./app');
 
-const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
-
-app.use((req, res, next) => {
-  if (req.method !== 'GET') {
-    return next();
-  }
-  res.sendFile(path.resolve(__dirname, '..', '..', 'dist', 'index.html'));
-});
-
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Started on http://localhost:3000');
 });
