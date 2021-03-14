98604c9640329a2ad532c8a505902392c0c438aa
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-03-15 00:34:20 +0100

    added tests for quiz.tsx

---
+++ src/tests/client/components/quiz-test.jsx
@@ -0,0 +1,48 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Quiz from '../../../client/components/quiz';
+
+test('testing if quiz returns true', async () => {
+  const quiz = {
+    question: 'what is absolute zero?',
+    answers: ['-300 Celsius', '0 Kelvin', '0 Fahrenheit', '0 Celsius'],
+    correct: 1,
+  };
+  let state;
+  const onClick = (x) => {
+    state = x;
+  };
+
+  render(<Quiz quiz={quiz} handleClick={onClick} />);
+  screen.getByText('Question: ' + quiz.question);
+  screen.getByText('A: ' + quiz.answers[0]);
+  screen.getByText('B: ' + quiz.answers[1]);
+  screen.getByText('C: ' + quiz.answers[2]);
+  screen.getByText('D: ' + quiz.answers[3]);
+  fireEvent.click(screen.getByText('B: ' + quiz.answers[1]));
+  expect(state).toBe(true);
+});
+
+test('testing if quiz returns fakse', async () => {
+  const quiz = {
+    question: 'what is absolute zero?',
+    answers: ['-300 Celsius', '0 Kelvin', '0 Fahrenheit', '0 Celsius'],
+    correct: 1,
+  };
+  let state;
+  const onClick = (x) => {
+    state = x;
+  };
+
+  render(<Quiz quiz={quiz} handleClick={onClick} />);
+  screen.getByText('Question: ' + quiz.question);
+  screen.getByText('A: ' + quiz.answers[0]);
+  screen.getByText('B: ' + quiz.answers[1]);
+  screen.getByText('C: ' + quiz.answers[2]);
+  screen.getByText('D: ' + quiz.answers[3]);
+  fireEvent.click(screen.getByText('A: ' + quiz.answers[0]));
+});
