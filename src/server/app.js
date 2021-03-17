6784abdb732f4150620243d1a782f217bc172e42
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-03-17 20:31:43 +0100

    added end-points for login/logout/signup/user

--- src/server/app.js
+++ src/server/app.js
@@ -3,12 +3,14 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const matchApi = require("./routes/match-api");
+const authApi = require("./routes/auth-api");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api", matchApi);
+app.use("/api", authApi);
 
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 
