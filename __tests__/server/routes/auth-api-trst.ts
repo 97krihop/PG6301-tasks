a0f1e8445553e79ba76ebf9ab7b87f10feedc762
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-03-24 21:19:02 +0100

    added tests and improvements login with sessions

---
+++ __tests__/server/routes/auth-api-trst.ts
@@ -0,0 +1,115 @@
+const request = require("supertest");
+const app = require("../../../src/server/app");
+
+let counter = 0;
+
+it("should fail to login", async () => {
+  const res = await request(app)
+    .post("/api/login")
+    .send({ username: "foo_" + counter++, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res.statusCode).toEqual(401);
+});
+
+it("should fail to get user", async () => {
+  const res = await request(app).get("/api/user");
+  expect(res.statusCode).toEqual(401);
+});
+
+it("should logout user", async () => {
+  const res = await request(app).post("/api/logout");
+  expect(res.statusCode).toEqual(204);
+});
+
+it("should create a user", async () => {
+  const res = await request(app)
+    .post("/api/signup")
+    .send({ username: "foo_" + counter++, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res.statusCode).toEqual(201);
+});
+
+it("should fail to create a user", async () => {
+  const res = await request(app)
+    .post("/api/signup")
+    .send({ username: "foo_" + counter, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res.statusCode).toEqual(201);
+
+  const res2 = await request(app)
+    .post("/api/signup")
+    .send({ username: "foo_" + counter++, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res2.statusCode).toEqual(400);
+});
+it("should create a user and login", async () => {
+  const res = await request(app)
+    .post("/api/signup")
+    .send({ username: "foo_" + counter, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res.statusCode).toEqual(201);
+
+  const res2 = await request(app)
+    .post("/api/login")
+    .send({ username: "foo_" + counter++, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res2.statusCode).toEqual(200);
+});
+it("should get a user and login", async () => {
+  const userId = "foo_" + counter++;
+
+  const agent = request.agent(app);
+
+  const res = await agent
+    .post("/api/signup")
+    .send({ username: userId, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res.statusCode).toEqual(201);
+
+  const res2 = await agent
+    .post("/api/login")
+    .send({ username: userId, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res2.statusCode).toEqual(200);
+
+  const {
+    statusCode,
+    body: { id, victories, defeats },
+  } = await agent.get("/api/user");
+  expect(statusCode).toEqual(200);
+  expect(id).toBe(userId);
+  expect(victories).toEqual(0);
+  expect(defeats).toEqual(0);
+});
+it("should get a user and login", async () => {
+  const userId = "foo_" + counter++;
+
+  const agent = request.agent(app);
+
+  const res = await agent
+    .post("/api/signup")
+    .send({ username: userId, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res.statusCode).toEqual(201);
+
+  const res2 = await agent
+    .post("/api/login")
+    .send({ username: userId, password: "bar" })
+    .set("Content-Type", "application/json");
+  expect(res2.statusCode).toEqual(200);
+
+  const {
+    statusCode,
+    body: { id, victories, defeats },
+  } = await agent.get("/api/user");
+  expect(statusCode).toEqual(200);
+  expect(id).toBe(userId);
+  expect(victories).toEqual(0);
+  expect(defeats).toEqual(0);
+
+  const res4 = await agent.post("/api/logout");
+  expect(res4.statusCode).toEqual(204);
+
+  const res5 = await agent.get("/api/user");
+  expect(res5.statusCode).toEqual(401);
+});
--- src/server/routes/auth-api.js
+++ src/server/routes/auth-api.js
@@ -13,23 +13,18 @@ router.post("/login", (req, res) => {
 
 router.post("/signup", (req, res) => {
   const { username, password } = req.body;
-  if (username.length > 4 && password.length > 6) {
-    const success = userDb.createUser(username, password);
-    if (!success) res.status(400).send();
-    res.status(201).send();
-  } else res.status(400).send();
+  const success = userDb.createUser(username, password);
+  if (!success) res.status(400).send();
+  res.status(201).send();
 });
 
 router.post("/logout", (req, res) => {
-  if (req.session) {
-    req.session.destroy();
-    return res.status(200).send();
-  }
-  res.status(400).send();
+  req.session.destroy();
+  res.status(204).send();
 });
 
 router.get("/user", (req, res) => {
-  if (!req.session) return res.status(401).send();
+  if (!req.session.username) return res.status(401).send();
   const user = userDb.getUser(req.session.username);
   res.status(200).json({
     id: user.id,
