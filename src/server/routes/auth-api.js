6cbd7b4af89671c7c4bd2316117566190d0fff47
Kristoffer <24739853+97krihop@users.noreply.github.com>
2021-04-07 15:22:56 +0200

    removed correct from api so people cant cheat

--- src/server/routes/auth-api.js
+++ src/server/routes/auth-api.js
@@ -1,31 +1,36 @@
 const express = require("express");
 const userDb = require("../db/users");
+const passport = require("passport");
 
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (userDb.verifyUser(username, password)) {
-    req.session.username = username;
-    res.end();
-  } else res.status(401).send();
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.status(204).send();
 });
 
 router.post("/signup", (req, res) => {
   const { username, password } = req.body;
   const success = userDb.createUser(username, password);
-  if (!success) res.status(400).send();
-  res.status(201).send();
+
+  if (!success) returnres.status(400).send();
+
+  passport.authenticate("local")(req, res, () => {
+    req.session.save((err) => {
+      if (err) res.status(500).send();
+      else res.status(201).send();
+    });
+  });
 });
 
 router.post("/logout", (req, res) => {
+  req.logout();
   req.session.destroy();
   res.status(204).send();
 });
 
 router.get("/user", (req, res) => {
-  if (!req.session.username) return res.status(401).send();
-  const user = userDb.getUser(req.session.username);
+  if (!req.user) return res.status(401).send();
+  const user = userDb.getUser(req.user);
   res.status(200).json({
     id: user.id,
     victories: user.victories,
